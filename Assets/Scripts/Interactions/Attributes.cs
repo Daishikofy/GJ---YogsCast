c29a5dfdd524dce06fadc295a17507cff4e071f4
Daishikofy <daishi.setsuko@gmail.com>
2019-12-14 22:03:24 -0300

    Added some interactions

---
+++ Assets/Scripts/Interactions/Attributes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributes : MonoBehaviour {
+    [System.Serializable]
+    public enum Animal
+    {
+        Chick,
+        Animal2,
+        Animal3
+    };
+
+    [System.Serializable]
+    public enum Biome
+    {
+        Hayfield,
+        Desert,
+        Island,
+        SnowyMountain
+    };
+
+    [System.Serializable]
+    public enum Food
+    {
+        normal,
+        Red,
+        Blue,
+        Purple
+    };
+}
+
---
+++ Assets/Scripts/Interactions/Pots.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pots : MonoBehaviour, Interactable
+{
+    [SerializeField]
+    Attributes.Biome biome;
+    [SerializeField]
+    float waterGrowthBoost;
+    [SerializeField]
+    float wateredTime;
+
+    float timeSinceWater;
+    bool isOccupied;
+    bool isWatered;
+    GameObject plant;
+    public void OnInteraction(Player player)
+    {
+        var selectedObject = player.selectedObject;
+        if (selectedObject != null)
+        {
+            if (selectedObject.isType(typeof(Seed).Name))
+                Debug.Log("You just planted a " + selectedObject.getType());
+            if (selectedObject.isType(typeof(WaterCan).Name))
+                Debug.Log("You just planted a " + selectedObject.getType());
+        }
+        else if (selectedObject == null)
+        {
+            //If the plant is ready, harvest
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            if (!isOccupied) return;
+    }
+
+    private void resetPot()
+    {
+        plant = null;
+        isWatered = false;
+    }
+
+    private void setWater(bool value)
+    {
+        if (value)
+        {
+            isWatered = value;
+
+        }
+    }
+    private IEnumerator waterCoolDown()
+    {
+        isWatered = true;
+        float cooldown = wateredTime;
+        while (cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+            yield return null;
+        }
+        isWatered = false;    
+    }
+}
